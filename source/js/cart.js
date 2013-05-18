a2d5f6d76bcfcd8587ec9f3f9631a868d878dd96
hqd <hqdtech@gmail.com>
2013-05-18 14:11:10 +0700

    Commit 02

---
+++ source/js/cart.js
@@ -0,0 +1,16 @@
+﻿$(document).delegate("#cart_page", "pageinit", function () {
+
+    setTimeout(function () {
+        InitTapEvent_ForPaymentButton();
+    }, 100);
+
+});
+
+function InitTapEvent_ForPaymentButton() {
+    $(".bg-buttonPayment").tap(function () {
+        
+        pageUrl = "checkout_address.html";
+        //alert(pageUrl + "|" + id);
+        $.mobile.changePage(pageUrl, { transition: "fade" });
+    });
+}
